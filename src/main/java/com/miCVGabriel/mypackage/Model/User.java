dd5407a2195806752b6066fe3b9078a1f35d4224
Gabs343 <gabriellopezp343@gmail.com>
2022-03-20 16:31:07 -0300

    columns parameters

--- src/main/java/com/miCVGabriel/mypackage/Model/User.java
+++ src/main/java/com/miCVGabriel/mypackage/Model/User.java
@@ -4,20 +4,28 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter @Setter @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Integer id;
+
+    @Column(name = "Name", length = 55, nullable = false)
     private String name;
+
+    @Column(name = "Img", nullable = false)
     private String img;
+
+    @Column(name = "Title", length = 155, nullable = false)
     private String title;
+
+    @Column(name = "Password", nullable = false)
     private String password;
+
+    @Column(name = "Description", nullable = false)
     private String description;
 }
