252bba9a8e49c04504afb7e2e7cc9066a5bf8da2
Gabs343 <gabriellopezp343@gmail.com>
2022-03-20 16:49:49 -0300

    connecting repository with model

--- src/main/java/com/miCVGabriel/mypackage/Repository/ProyectRepository.java
+++ src/main/java/com/miCVGabriel/mypackage/Repository/ProyectRepository.java
@@ -1,4 +1,9 @@
 package com.miCVGabriel.mypackage.Repository;
 
-public interface ProyectRepository {
+import com.miCVGabriel.mypackage.Model.Proyect;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProyectRepository extends JpaRepository<Proyect, Integer> {
 }
