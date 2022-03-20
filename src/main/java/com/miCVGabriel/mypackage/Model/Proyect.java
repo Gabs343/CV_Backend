c2b197c25451c179e0416a3052a1c28f3db123a5
Gabs343 <gabriellopezp343@gmail.com>
2022-03-20 16:41:09 -0300

    Proyect model

---
+++ src/main/java/com/miCVGabriel/mypackage/Model/Proyect.java
@@ -0,0 +1,35 @@
+package com.miCVGabriel.mypackage.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+public class Proyect {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "Name", length = 55, nullable = false)
+    private String name;
+
+    @Column(name = "Img", nullable = false)
+    private String img;
+
+    @Column(name = "Description", nullable = false)
+    private String description;
+
+    @Column(name = "Link")
+    private String link;
+
+    @Column(name = "Finished", nullable = false)
+    private Boolean finished;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "userID", nullable = false, referencedColumnName = "ID")
+    private User user;
+}
