eeb6ca27374715dcae20ab1ca0196983393e71c8
Gabs343 <gabriellopezp343@gmail.com>
2022-03-20 21:12:32 -0300

    mapping the methods

--- src/main/java/com/miCVGabriel/mypackage/Controller/ProyectController.java
+++ src/main/java/com/miCVGabriel/mypackage/Controller/ProyectController.java
@@ -1,2 +1,47 @@
-package com.miCVGabriel.mypackage.Controller;public class ProyectController {
+package com.miCVGabriel.mypackage.Controller;
+
+import com.miCVGabriel.mypackage.Model.Proyect;
+import com.miCVGabriel.mypackage.Service.IProyectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProyectController {
+    @Autowired
+    private IProyectService iProyectService;
+
+    @GetMapping("/proyectsByUser/{userID}")
+    public List<Proyect> findProyectsByUser(@PathVariable Integer userID){
+        return iProyectService.getProyectsByUser(userID);
+    }
+
+    @PostMapping("/proyect/create")
+    public String createProyect(@RequestBody Proyect p){
+        iProyectService.saveProyect(p);
+        return "proyect saved";
+    }
+
+    @PutMapping("/proyect/edit/{id}")
+    public Proyect editProyect(@PathVariable Integer id, @RequestBody Proyect p){
+        Proyect editedProyect = iProyectService.getProyectByID(id);
+
+        editedProyect.setImg(p.getImg());
+        editedProyect.setName(p.getName());
+        editedProyect.setFinished(p.getFinished());
+        editedProyect.setLink(p.getLink());
+        editedProyect.setDescription(p.getDescription());
+        editedProyect.setUser(p.getUser());
+
+        iProyectService.saveProyect(editedProyect);
+
+        return editedProyect;
+    }
+
+    @DeleteMapping("/proyect/delete/{id}")
+    public String deleteProyect(@PathVariable Integer id){
+        iProyectService.deleteProyect(id);
+        return "proyect deleted";
+    }
 }
