18b6b9db7218f846f5662b9897f1a11f23a67c89
Gabs343 <gabriellopezp343@gmail.com>
2022-03-23 19:27:29 -0300

    methods for the social network service

--- src/main/java/com/miCVGabriel/mypackage/service/SocialNetworkService.java
+++ src/main/java/com/miCVGabriel/mypackage/service/SocialNetworkService.java
@@ -1,4 +1,40 @@
 package com.miCVGabriel.mypackage.service;
 
-public class SocialNetworkService {
+import com.miCVGabriel.mypackage.model.SocialNetwork;
+import com.miCVGabriel.mypackage.model.User;
+import com.miCVGabriel.mypackage.repository.SocialNetworkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SocialNetworkService implements ISocialNetworkService{
+    @Autowired
+    private SocialNetworkRepository socialNetworkRepository;
+
+    @Override
+    public List<SocialNetwork> getSocialNetworksByUser(User u){
+        List<SocialNetwork> socialNetworksByUser = new ArrayList<>();
+
+        socialNetworkRepository.findSocialNetworkByUser(u).forEach(socialNetworksByUser::add);
+
+        return socialNetworksByUser;
+    }
+
+    @Override
+    public SocialNetwork getSocialNetworkByID(Integer id){
+        return socialNetworkRepository.getById(id);
+    }
+
+    @Override
+    public void saveSocialNetwork(SocialNetwork socialNet){
+        socialNetworkRepository.save(socialNet);
+    }
+
+    @Override
+    public void deleteSocialNetwork(Integer id){
+        socialNetworkRepository.deleteById(id);
+    }
 }
