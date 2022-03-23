3379fbc0d3a7362bc4a53ccc2f0d40b056adfe65
Gabs343 <gabriellopezp343@gmail.com>
2022-03-22 21:33:22 -0300

    List of social networks that have the user

--- src/main/java/com/miCVGabriel/mypackage/repository/SocialNetworkRepository.java
+++ src/main/java/com/miCVGabriel/mypackage/repository/SocialNetworkRepository.java
@@ -1,2 +1,13 @@
-package com.miCVGabriel.mypackage.repository;public interface SocialNetworkRepository {
+package com.miCVGabriel.mypackage.repository;
+
+import com.miCVGabriel.mypackage.model.SocialNetwork;
+import com.miCVGabriel.mypackage.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SocialNetworkRepository extends JpaRepository<SocialNetwork, Integer> {
+    public List<SocialNetwork> findBySocialNetworkByUser(User user);
 }
