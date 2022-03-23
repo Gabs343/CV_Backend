5ddd02b865459105f01312f340658d92c05f5961
Gabs343 <gabriellopezp343@gmail.com>
2022-03-22 21:23:38 -0300

    Columns for the table

--- src/main/java/com/miCVGabriel/mypackage/model/SocialNetwork.java
+++ src/main/java/com/miCVGabriel/mypackage/model/SocialNetwork.java
@@ -1,2 +1,31 @@
-package com.miCVGabriel.mypackage.model;public class SocialNetwork {
+package com.miCVGabriel.mypackage.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+public class SocialNetwork {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "ID")
+   private Integer id;
+
+   @Column(name = "Name", length = 55, nullable = false)
+   private String name;
+
+   @Column(name = "Link", nullable = false)
+   private String link;
+
+   @Column(name = "Img", nullable = false)
+   private String img;
+
+   @ManyToOne(optional = false, fetch = FetchType.LAZY)
+   @JoinColumn(name = "userID", nullable = false, referencedColumnName = "id")
+   @JsonBackReference
+   private User user;
 }
