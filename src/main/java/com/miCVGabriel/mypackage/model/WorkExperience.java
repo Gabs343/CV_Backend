29debb4eaf7f31f5e2f1e65c607ac2fd67c8cec4
Gabs343 <gabriellopezp343@gmail.com>
2022-03-23 22:55:15 -0300

    attributes for work experience

--- src/main/java/com/miCVGabriel/mypackage/model/WorkExperience.java
+++ src/main/java/com/miCVGabriel/mypackage/model/WorkExperience.java
@@ -1,15 +1,44 @@
 package com.miCVGabriel.mypackage.model;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Experience {
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter @Setter @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+public class WorkExperience {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private Integer id;
 
-    
+    @Column(name = "Charge_Name", length = 75, nullable = false)
+    private String chargeName;
+
+    @Column(name = "Company", length = 75, nullable = false)
+    private String company;
+
+    @Column(name = "Img", nullable = false)
+    private String img;
+
+    @Column(name = "Entry", nullable = false)
+    private Date entry;
+
+    @Column(name = "Exit")
+    private Date exit;
+
+    @Column(name = "Current", nullable = false)
+    private Boolean current;
+
+    @Column(name = "Description", nullable = false)
+    private String description;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "userID", nullable = false, referencedColumnName = "id")
+    @JsonBackReference
+    private  User user;
 }
