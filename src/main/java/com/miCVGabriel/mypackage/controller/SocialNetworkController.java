fb39745d36f2c71110eaa3ddad83844182a89b25
Gabs343 <gabriellopezp343@gmail.com>
2022-03-23 21:40:37 -0300

    Mapping methods in the social network controller

--- src/main/java/com/miCVGabriel/mypackage/controller/SocialNetworkController.java
+++ src/main/java/com/miCVGabriel/mypackage/controller/SocialNetworkController.java
@@ -1,2 +1,46 @@
-package com.miCVGabriel.mypackage.controller;public class SocialNetworkController {
+package com.miCVGabriel.mypackage.controller;
+
+import com.miCVGabriel.mypackage.model.SocialNetwork;
+import com.miCVGabriel.mypackage.model.User;
+import com.miCVGabriel.mypackage.service.ISocialNetworkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class SocialNetworkController {
+    @Autowired
+    private ISocialNetworkService iSocialNetService;
+
+    @GetMapping("/socialNetworksByUser/{userID}")
+    public List<SocialNetwork> findSocialNetworksByUser(@PathVariable User userID){
+        return iSocialNetService.getSocialNetworksByUser(userID);
+    }
+
+    @PostMapping("/socialNetworks")
+    public String createSocialNetwork(@RequestBody SocialNetwork socialNet){
+        iSocialNetService.saveSocialNetwork(socialNet);
+        return "social network saved";
+    }
+
+    @PutMapping("/socialNetworks/{id}")
+    public SocialNetwork editSocialNetwork(@PathVariable Integer id, @RequestBody SocialNetwork socialNet){
+        SocialNetwork editedSocialNet = iSocialNetService.getSocialNetworkByID(id);
+
+        editedSocialNet.setName(socialNet.getName());
+        editedSocialNet.setLink(socialNet.getLink());
+        editedSocialNet.setImg(socialNet.getImg());
+        editedSocialNet.setUser(socialNet.getUser());
+
+        iSocialNetService.saveSocialNetwork(editedSocialNet);
+
+        return editedSocialNet;
+    }
+
+    @DeleteMapping("/socialNetworks/{id}")
+    public String deleteSocialNetwork(@PathVariable Integer id){
+        iSocialNetService.deleteSocialNetwork(id);
+        return "social network deleted";
+    }
 }
