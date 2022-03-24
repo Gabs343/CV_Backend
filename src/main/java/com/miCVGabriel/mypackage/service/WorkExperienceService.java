7918f0b4e735f206ff2602883c734d203e3a5f5f
Gabs343 <gabriellopezp343@gmail.com>
2022-03-24 17:22:29 -0300

    override for service methods

--- src/main/java/com/miCVGabriel/mypackage/service/WorkExperienceService.java
+++ src/main/java/com/miCVGabriel/mypackage/service/WorkExperienceService.java
@@ -1,2 +1,40 @@
-package com.miCVGabriel.mypackage.service;public class WorkExperienceService {
+package com.miCVGabriel.mypackage.service;
+
+import com.miCVGabriel.mypackage.model.User;
+import com.miCVGabriel.mypackage.model.WorkExperience;
+import com.miCVGabriel.mypackage.repository.WorkExperienceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class WorkExperienceService implements IWorkExperienceService {
+    @Autowired
+    private WorkExperienceRepository workExpRepository;
+
+    @Override
+    public Set<WorkExperience> getWorkExperiencesByUser(User userID){
+        Set<WorkExperience> workExpsByUser = new HashSet<>();
+
+        workExpRepository.findWorkExperiencesByUser(userID).forEach(workExpsByUser::add);
+
+        return workExpsByUser;
+    }
+
+    @Override
+    public WorkExperience getWorkExperienceByID(Integer id){
+       return  workExpRepository.getById(id);
+    }
+
+    @Override
+    public void saveWorkExperience(WorkExperience workExp){
+        workExpRepository.save(workExp);
+    }
+
+    @Override
+    public void deleteWorkExperience(Integer id){
+        workExpRepository.deleteById(id);
+    }
 }
