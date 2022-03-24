0f05b5a446ff48daacecc4a593a65fe6bee43afe
Gabs343 <gabriellopezp343@gmail.com>
2022-03-24 17:08:57 -0300

    methods for the interface work experience

--- src/main/java/com/miCVGabriel/mypackage/service/IWorkExperience.java
+++ src/main/java/com/miCVGabriel/mypackage/service/IWorkExperience.java
@@ -1,2 +1,16 @@
-package com.miCVGabriel.mypackage.service;public interface IWorkExperience {
+package com.miCVGabriel.mypackage.service;
+
+import com.miCVGabriel.mypackage.model.User;
+import com.miCVGabriel.mypackage.model.WorkExperience;
+
+import java.util.Set;
+
+public interface IWorkExperience {
+    public Set<WorkExperience> getWorkExperiencesByUser(User userID);
+
+    public WorkExperience getWorkExperienceByID(Integer id);
+
+    public void saveWorkExperience(WorkExperience workExp);
+
+    public void deleteWorkExperience(Integer id);
 }
