ed7b4f374919e904050894317935ab306b40033d
Gabs343 <gabriellopezp343@gmail.com>
2022-03-24 17:00:30 -0300

    set experiences by user

--- src/main/java/com/miCVGabriel/mypackage/repository/WorkExperienceRepository.java
+++ src/main/java/com/miCVGabriel/mypackage/repository/WorkExperienceRepository.java
@@ -1,4 +1,13 @@
 package com.miCVGabriel.mypackage.repository;
 
-public interface WorkExperienceRepository {
+import com.miCVGabriel.mypackage.model.User;
+import com.miCVGabriel.mypackage.model.WorkExperience;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface WorkExperienceRepository extends JpaRepository<WorkExperience, Integer> {
+    public Set<WorkExperience> findWorkExperienceByUser(User user);
 }
