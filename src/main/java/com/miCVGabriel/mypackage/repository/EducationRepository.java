4dd0e3ebf4c75f1f02b940a72661deb4e78eef87
Gabs343 <gabriellopezp343@gmail.com>
2022-03-24 23:04:45 -0300

    method in repository

--- src/main/java/com/miCVGabriel/mypackage/repository/EducationRepository.java
+++ src/main/java/com/miCVGabriel/mypackage/repository/EducationRepository.java
@@ -1,2 +1,11 @@
-package com.miCVGabriel.mypackage.repository;public interface EducationRepository {
+package com.miCVGabriel.mypackage.repository;
+
+import com.miCVGabriel.mypackage.model.Education;
+import com.miCVGabriel.mypackage.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+
+public interface EducationRepository extends JpaRepository<Education, Long> {
+    public Set<Education> findEducationsByUser(User user);
 }
