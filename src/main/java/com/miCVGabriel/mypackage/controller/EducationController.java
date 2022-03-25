6434def684c2dfb8f19394651b84a324e71d15cc
Gabs343 <gabriellopezp343@gmail.com>
2022-03-25 00:00:38 -0300

    mapping the methods

--- src/main/java/com/miCVGabriel/mypackage/controller/EducationController.java
+++ src/main/java/com/miCVGabriel/mypackage/controller/EducationController.java
@@ -1,2 +1,43 @@
-package com.miCVGabriel.mypackage.controller;public class EducationController {
+package com.miCVGabriel.mypackage.controller;
+
+import com.miCVGabriel.mypackage.model.Education;
+import com.miCVGabriel.mypackage.service.IEducationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class EducationController {
+    @Autowired
+    private IEducationService iEducationService;
+
+    @PostMapping("/educations")
+    public String createEducation(@RequestBody Education education){
+        iEducationService.saveEducation(education);
+        return "education saved";
+    }
+
+    @PutMapping("/educations/{id}")
+    public Education editedEducation(@PathVariable Long id, @RequestBody Education education){
+        Education editedEducation = iEducationService.getEducationByID(id);
+
+        editedEducation.setTitle(education.getTitle());
+        editedEducation.setInstitution(education.getInstitution());
+        editedEducation.setImg(education.getImg());
+        editedEducation.setEntry(education.getEntry());
+        editedEducation.setWayOut(education.getWayOut());
+        editedEducation.setPresentDay(education.getPresentDay());
+        editedEducation.setFinished(education.getFinished());
+        editedEducation.setDescription(education.getDescription());
+        editedEducation.setUser(education.getUser());
+
+        iEducationService.saveEducation(editedEducation);
+
+        return editedEducation;
+    }
+
+    @DeleteMapping("/educations/{id}")
+    public String deleteEducation(@PathVariable Long id){
+        iEducationService.deleteEducation(id);
+        return "education deleted";
+    }
 }
